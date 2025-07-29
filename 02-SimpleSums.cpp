9451ce33a7f3ae6c3273d3300e8a6e85519467d9
su120303 <suhad120303@gmail.com>
2025-07-29 18:53:11 +0100

    Create 02-SimpleSums.cpp
    Exercise 2

---
+++ 02-SimpleSums.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 8;
+    int num2 = 10;
+    int sum = num1 + num2;
+
+    cout << "The sum is: " << sum << endl;
+    return 0;
+}
