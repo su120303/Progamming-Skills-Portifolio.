267bfdf968bd8bb4a64a91aae3dc2177c9bd79a9
su120303 <suhad120303@gmail.com>
2025-07-29 18:57:25 +0100

    Create 03-Biography.cpp
    Exercise 4

---
+++ 03-Biography.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "Suhad Erfan";
+    string hometown = "Lindesberg";
+    int age = 22;
+
+    cout << name << "\n" << hometown << "\n" << age << endl;
+    return 0;
+}
