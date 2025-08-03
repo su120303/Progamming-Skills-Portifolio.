2324d2804e1406c27a525bf09d13a688ee02d269
su120303 <suhad120303@gmail.com>
2025-08-03 20:09:08 +0100

    Update 01-Coding-Is-Cool.cpp

--- 01-Coding-Is-Cool.cpp
+++ 01-Coding-Is-Cool.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-#include <string>
+/*
+    Exercise 01 - Coding is Cool
+    ----------------------------
+    This is a simple introductory program to confirm you can output a message
+    to the console. It uses the standard output stream to display "Coding is Cool".
+
+    Purpose: Demonstrate use of basic output in C++.
+*/
+
+#include <iostream>  // For input/output stream
+#include <string>    // For using string data type (not used here, but often pre-included)
+
 using namespace std;
 
 int main() {
+    // Output the message to the console
     cout << "Coding is Cool" << endl;
+
+    return 0; // Indicate the program ran successfully
 }
