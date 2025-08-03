a15e27320c65a9800f1d52881b57816306308f93
su120303 <suhad120303@gmail.com>
2025-08-03 20:32:37 +0100

    Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,23 @@
+/*
+    Exercise 09 - Hello
+    -------------------
+    This program defines a simple function called hello()
+    which prints "Hello" to the console.
+
+    Purpose: Demonstrates how to define and call a function in C++.
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function definition
+void hello() {
+    cout << "Hello" << endl;
+}
+
+int main() {
+    // Call the hello function
+    hello();
+
+    return 0;
+}
